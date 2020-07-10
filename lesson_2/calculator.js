3807898f86e0db9cba21671d8e123257989a1e02
Matt Rhone <kritoke@users.noreply.github.com>
2020-07-10 05:29:05 -0500

    Fix a spacing issue.

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -35,7 +35,7 @@ while (invalidNumber(number2)) {
 prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
 
-while(!['1', '2', '3', '4'].includes(operation)) {
+while (!['1', '2', '3', '4'].includes(operation)) {
   prompt('Must choose 1, 2, 3 or 4');
   operation = readline.question();
 }
