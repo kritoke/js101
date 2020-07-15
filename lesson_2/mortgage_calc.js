e799bc1fa2cd2db9c72422e0eadb59a364a82411
Matt Rhone <kritoke@users.noreply.github.com>
2020-07-15 12:38:56 -0500

    initial scaffold, pull info from user

---
+++ lesson_2/mortgage_calc.js
@@ -0,0 +1,23 @@
+// get loan amount, APR, and loan duration from the user
+// calculate monthly interest rate
+// calculation loan duration in months
+// use let m = p * (j / (1 - Math.pow((1 + j), (-n)))); to calculate the final number
+
+const readline = require('readline-sync');
+
+function prompt(message) {
+    console.log(`=> ${message}`);
+}
+
+prompt("What is the loan amount?");
+let loanAmount = readline.question();
+
+prompt("What is the APR of the loan?");
+let apr = readline.question();
+
+prompt("What is the loan duration in months?");
+let loanDuration = readline.question();
+
+
+
+console.log(loanAmount);
