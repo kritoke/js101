59e3c42b50cdc9b7f61acee75f44a82af7cad23a
Matt Rhone <3081486+kritoke@users.noreply.github.com>
2020-08-21 06:08:32 -0500

    Fix styling to pass eslint.

--- lesson_2/mortgage_calc.js
+++ lesson_2/mortgage_calc.js
@@ -1,12 +1,13 @@
 // get loan amount, APR, and loan duration from the user
 // calculate monthly interest rate
 // calculation loan duration in months
-// use let m = p * (j / (1 - Math.pow((1 + j), (-n)))); to calculate the final number
+// use let m = p * (j / (1 - Math.pow((1 + j),
+// (-n)))); to calculate the final number
 
 const readline = require('readline-sync');
 
 function prompt(message) {
-    console.log(`=> ${message}`);
+  console.log(`=> ${message}`);
 }
 
 prompt("What is the loan amount?");
@@ -20,6 +21,8 @@ let monthlyInterestRate = apr / 12;
 prompt("What is the loan duration in months?");
 let loanDuration = readline.question();
 
-let monthlyPayment = (loanAmount * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-loanDuration))))).toFixed(2);
+let monthlyPayment =
+  (loanAmount * (monthlyInterestRate /
+    (1 - Math.pow((1 + monthlyInterestRate), (-loanDuration))))).toFixed(2);
 
 prompt(`The monthly payment is $${monthlyPayment}`);
