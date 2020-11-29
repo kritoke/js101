72c924b20eeb251f31708601de105cc35a02ca2e
Matt <matthew.rhone@outlook.com>
2020-11-29 15:52:39 -0600

    Add invalid number fn and add it to each input option

--- lesson_2/mortgage_calc.js
+++ lesson_2/mortgage_calc.js
@@ -10,16 +10,35 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
 prompt("What is the loan amount?");
 let loanAmount = readline.question();
+while (invalidNumber(loanAmount))
+{
+  prompt("Number must be valid, please enter a new loan amount.")
+  loanAmount = readline.question();
+}
 
 prompt("What is the APR of the loan? (i.e. 10 for 10%)");
-let apr = readline.question() / 100;
+let apr = readline.question();
+while (invalidNumber(apr))
+{
+  prompt("Number must be valid, please enter a new APR.")
+  apr = readline.question();
+}
 
-let monthlyInterestRate = apr / 12;
+let monthlyInterestRate = (apr / 100) / 12;
 
 prompt("What is the loan duration in months?");
 let monthlyDuration = readline.question();
+while (invalidNumber(monthlyDuration))
+{
+  prompt("Number must be valid, please enter a new monthly duration.")
+  monthlyDuration = readline.question();
+}
 
 let monthlyPayment =
   (loanAmount * (monthlyInterestRate /
