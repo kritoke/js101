f6b09c41305b2615db8b33dc87191e0c4e9817f9
kritoke <kritoke@localhost.localdomain>
2020-12-06 12:19:03 -0600

    Fix exercise2, add exercise3.

--- small_problems/easy_1/exercise2.js
+++ small_problems/easy_1/exercise2.js
@@ -1,6 +1,6 @@
 // Log all odd numbers from 1 to 99, inclusive, to the console,
 // with each number on a separate line.
 
-for (let num = 0; num < 100; num++) {
+for (let num = 1; num < 100; num++) {
   if (num % 2 !== 0) console.log(num);
 }
