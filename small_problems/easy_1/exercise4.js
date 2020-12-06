0932f145a95798324c2692c5640d74a2c443cec6
kritoke <kritoke@localhost.localdomain>
2020-12-06 12:33:35 -0600

    add exercise 4.

---
+++ small_problems/easy_1/exercise4.js
@@ -0,0 +1,23 @@
+/*
+Build a program that asks the user to enter the length and width 
+of a room in meters, and then logs the area of the room to the console
+ in both square meters and square feet.
+*/
+
+const readline = require('readline-sync');
+const METERS_IN_FEET = 10.7639;
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+prompt("Enter the length of the room in meters:");
+let roomLength = Number(readline.question());
+
+prompt("Enter the width of the room in meters:");
+let roomWidth = Number(readline.question());
+
+let roomAreaMeters = roomLength * roomWidth;
+let roomAreaFeet = roomAreaMeters * METERS_IN_FEET;
+
+prompt(`The area of the room is ${roomAreaMeters} square meters (${roomAreaFeet.toFixed(2)} square feet).`);
